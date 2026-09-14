09a127aa1df9b42fd28c0070d6c37236df381704
alexokin <alex.okin@gmail.com>
2026-09-14 10:22:11 +0300

    Add basePath/assetPrefix to next.config.ts (code change, temporary)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: "/InvestorsPath",
+  assetPrefix: "/InvestorsPath/",
 };
 
 export default nextConfig;
